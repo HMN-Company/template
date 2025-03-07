b893e4c32ab1a4446a8f8e3207c698676ea09daf
hungnguyen988 <hungnguuen988@gmail.com>
2025-03-07 17:09:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Layout from "./component/layout/Layout";
 import HomePage from "./component/home/home_page/HomePage";
 import ProductsComponent from "./component/products/ProductsComponent";
 import IntroduceComponent from "./component/introduce/IntroduceComponent";
+import PreviewPage from "./component/preview_page/PreviewPage";
+import ContactPage from "./component/contact/ContactPage";
+import ServicePage from "./component/service_page/ServicePage";
 
 function App() {
   return (
@@ -14,8 +17,11 @@ function App() {
           <Route index element={<HomePage />} />  {/* Trang chủ */}
           <Route path="/products" element={<ProductsComponent />} />
           <Route path="/introduce" element={<IntroduceComponent />} />
+          <Route path="/contact-order" element={<ContactPage />} />
+          <Route path="/service" element={<ServicePage />} />
 
         </Route>
+          <Route path="/preview" element={<PreviewPage />} />
       </Routes>
     </div>
   );
